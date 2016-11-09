24a7d35313d619df6e4d9d2f944cf7d47d59f074
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-11-09 17:36:24 +0100

    Add test code to try out imported d3 module

--- src/helloWorld.component.ts
+++ src/helloWorld.component.ts
@@ -1,11 +1,19 @@
 import {
-  Component
+  Component,
+  OnInit
 } from '@angular/core';
+
+import d3 from './d3';
 
 @Component({
   selector: 'hello-world',
   template: 'Hello world from the {{ projectTitle }} module!'
 })
-export class HelloWorld {
+export class HelloWorld implements OnInit {
   projectTitle: string = 'weighted screen';
+
+  ngOnInit() {
+    d3.select("body").transition()
+    .style("background-color", "black");
+  }
 }
