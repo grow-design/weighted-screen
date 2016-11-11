4214ee1b67afbc1cacf8beb6812f58e1045c43cf
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-11-11 20:37:59 +0100

    Refactoring component with newly created service

--- src/weightedScreen.component.ts
+++ src/weightedScreen.component.ts
@@ -1,16 +1,17 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  Input
 } from '@angular/core';
 
 import d3 from './d3';
 
+import { WeightedScreenService } from './weighted-screen.service';
 
 @Component({
   selector: 'weighted-screen',
   template: `
-  <div id="erd">
-  </div>
+  <div id="erd"></div>
   `,
   styles: [
     `
@@ -24,68 +25,88 @@ import d3 from './d3';
 export class HelloWorld implements OnInit {
   projectTitle: string = 'weighted screen';
 
-  ngOnInit() {
-    var tree = `{
-        "id": 1,
-        "name": "tree",
-        "children": [
-            { "id": 3, "name": "A Day in the Life of a Product Owner", "views": 48, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/A+Day+in+the+Life+of+a+Product+Owner" },
-            { "id": 4, "name": "Advanced System Architecture", "views": 21, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Advanced+System+Architecture" },
-            { "id": 5, "name": "Annotations in Liferay portal", "views": 194, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Annotations+in+Liferay+portal" },
-            { "id": 6, "name": "Basic cluster config", "views": 41, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Basic+cluster+config" },
-            { "id": 7, "name": "Cloud DB", "views": 155, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Cloud+DB" },
-            { "id": 2, "name": "Clustering Basics", "views": 47, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Clustering+Basics" },
-            { "id": 8, "name": "Company Meeting Q video collection", "views": 74, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Company+Meeting+Q+video+collection" },
-            { "id": 9, "name": "CS & TS workflow", "views": 77, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/CS+&+TS+workflow" },
-            { "id": 10, "name": "Data Dialogs 2016", "views": 185, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Data+Dialogs+2016" },
-            { "id": 11, "name": "Eclipse Hot Keys", "views": 41, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Eclipse+Hot+Keys" }
-        ]
-    }`;
+  nodesRoot: Object;
 
-    var width = 1400,
-    height = 600;
+  @Input() nodesNumber: number = 10;
 
-    var color = d3.scales.scaleOrdinal()
-        .range(d3.scales.schemeCategory10
-            .map(function(c) { c = d3.color.rgb(c); c.opacity = 0.6; return c; }));
 
-    var root = JSON.parse( tree );
+  constructor(private weightedScreenService: WeightedScreenService) {}
 
-    root = d3.hierarchy.hierarchy(root);
-    root.sum(function(d) { return d.views});
+  getNodes(): void {
+    this.weightedScreenService.getNodesByViews(this.nodesNumber).then(
+      nodes => {
+        let rootNodes = {
+          "children": nodes
+        };
 
-    var treemap = d3.hierarchy.treemap()
-        .size([width, height])
-        .padding(1);
+        this.nodesRoot = rootNodes;
 
-    treemap(root);
+         var width = 1400,
+      height = 600;
 
-    d3.select("#erd")
-      .selectAll(".node")
-      .data(root.leaves())
-      .enter().append("div")
-        .attr("class", "node")
-        .attr("title", function(d) { return d.id; })
-        .style("left", function(d) { return d.x0 + "px"; })
-        .style("top", function(d) { return d.y0 + "px"; })
-        .style("width", function(d) { return d.x1 - d.x0 + "px"; })
-        .style("height", function(d) { return d.y1 - d.y0 + "px"; })
-        .style("line-height", function(d) { return d.y1 - d.y0 + "px"; })
-        .style("text-align", "center")
-        .style("background",
-          function(d) { 
-            return color(d.id); 
+      var color = d3.scales.scaleOrdinal()
+          .range(d3.scales.schemeCategory10
+              .map(function(c) { c = d3.color.rgb(c); c.opacity = 0.6; return c; }));
+
+      var root = d3.hierarchy.hierarchy(this.nodesRoot);
+  
+      root.sum(function(d) { return d.views});
+
+      var treemap = d3.hierarchy.treemap()
+          .size([width, height])
+          .padding(1);
+
+      treemap(root);
+
+      d3.select("#erd")
+          .selectAll(".node")
+          .data(root.leaves())
+          .enter()
+        .append("div")
+          .attr("class", "node")
+          .attr("title",
+            function(d) {
+              return d.id;
+            })
+          .style("left",
+            function(d) {
+              return d.x0 + "px";
+            })
+          .style("top",
+            function(d) {
+              return d.y0 + "px";
+            })
+          .style("width",
+            function(d) {
+              return d.x1 - d.x0 + "px";
+            })
+          .style("height",
+            function(d) {
+              return d.y1 - d.y0 + "px";
+            })
+          .style("line-height",
+            function(d) {
+              return d.y1 - d.y0 + "px";
+            })
+          .style("text-align", "center")
+          .style("background",
+            function(d) { 
+              return color(d.id); 
+            })
+        .append("div")
+          .attr("class", "node-label")
+          .style("height", function(d) { return d.y1 - d.y0 + "px"; })
+          .style("line-height", "inherit")
+        .append('a')
+          .attr("href", function(d){return d.data.url;})
+          .text(
+            function(d) { 
+              return d.data.name;
           })
-      .append("div")
-        .attr("class", "node-label")
-        .style("height", function(d) { return d.y1 - d.y0 + "px"; })
-        .style("line-height", "inherit")
-       .append('a')
-        .attr("href", function(d){return d.data.url;})
-         .text(
-          function(d) { 
-            return d.data.name;
-        })
+      })
+  }
 
+  ngOnInit() {
+    this.getNodes();
   }
 }
--- src/weightedScreen.module.ts
+++ src/weightedScreen.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HelloWorld} from './weightedScreen.component';
+import { WeightedScreenService }   from './weighted-screen.service';
 
 @NgModule({
   declarations: [
     HelloWorld
   ],
   imports: [CommonModule],
-  exports: [HelloWorld]
+  exports: [HelloWorld],
+  providers: [WeightedScreenService]
 })
 export class WeightedScreenModule {}
