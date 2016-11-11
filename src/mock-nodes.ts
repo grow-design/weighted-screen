827df03f0d9e89217bc3ffbed102a1e48efddc6e
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-11-11 20:37:11 +0100

    Add mock data

---
+++ src/mock-nodes.ts
@@ -0,0 +1,15 @@
+import { Node } from './node.model';
+
+export const NODES: Node[] = 
+[
+    { "id": 3, "name": "A Day in the Life of a Product Owner", "views": 48, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/A+Day+in+the+Life+of+a+Product+Owner" },
+    { "id": 4, "name": "Advanced System Architecture", "views": 21, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Advanced+System+Architecture" },
+    { "id": 5, "name": "Annotations in Liferay portal", "views": 14, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Annotations+in+Liferay+portal" },
+    { "id": 6, "name": "Basic cluster config", "views": 41, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Basic+cluster+config" },
+    { "id": 7, "name": "Cloud DB", "views": 155, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Cloud+DB" },
+    { "id": 2, "name": "Clustering Basics", "views": 47, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Clustering+Basics" },
+    { "id": 8, "name": "Company Meeting Q video collection", "views": 74, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Company+Meeting+Q+video+collection" },
+    { "id": 9, "name": "CS & TS workflow", "views": 77, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/CS+&+TS+workflow" },
+    { "id": 10, "name": "Data Dialogs 2016", "views": 185, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Data+Dialogs+2016" },
+    { "id": 11, "name": "Eclipse Hot Keys", "views": 41, "url": "https://grow.liferay.com/group/guest/learn/-/wiki/Grow/Eclipse+Hot+Keys" }
+]
