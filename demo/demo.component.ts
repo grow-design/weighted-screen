c418168f9e7f25951f9d354d035d939f6ace84a4
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-11-11 14:25:16 +0100

    Add needed styles

--- demo/demo.component.ts
+++ demo/demo.component.ts
@@ -3,5 +3,13 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'demo-app',
   template: '<weighted-screen></weighted-screen>',
+  styles: [
+    `
+    :host {
+      border: solid 1px #eee;
+      border-bottom: solid 1px #ccc;
+    }
+    `
+  ]
 })
 export class Demo {}
